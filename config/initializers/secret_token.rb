737049c88aca180d23c1b4d5a899b03864d5433b
Laura Williams <lwilliams@LauraWilliams.local>
2012-02-03 21:43:48 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CharitaBull::Application.config.secret_token = 'ab52f239070940051f429cf7755c9b6cf1028ac904b841b15d74127544b08c34d756cf93e5c4462ae83c949b6e27738b7dc5cedd968875988a7888cadfab957c'
